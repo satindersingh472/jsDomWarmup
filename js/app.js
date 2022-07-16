5be5e26b5e5695720f8f671e9fd2f6da9fe19983
satinder <satindersingh472@gmail.com>
2022-07-16 09:53:52 -0600

    finished

---
+++ js/app.js
@@ -0,0 +1,25 @@
+let some_id = document.getElementById(`name`);
+some_id[`style`][`backgroundColor`] = `green`;
+some_id[`style`][`fontSize`] = `2rem`;
+
+let class_same = document.querySelectorAll(`.sports`);
+let count = 0;
+for (count = 0; count < class_same.length; count++) {
+    class_same[count][`innerHTML`] = class_same[count][`innerHTML`] + `  This is the text`;
+}
+
+let class_image = document.getElementsByClassName(`image`);
+let image_count = 0;
+while (image_count < class_image.length) {
+    class_image[image_count].setAttribute(`src`, `https://images.unsplash.com/photo-1657915118196-7e4091a522ca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80`);
+    image_count++;
+}
+
+let p_tag = document.getElementsByTagName(`p`);
+let count_p = 0;
+for(count_p=0; count_p < p_tag.length; count_p++){
+    let does_include = p_tag[count_p][`innerHTML`].includes(`link`);
+    if(does_include === true){
+       p_tag[count_p][`innerHTML`] += `<a href="https://google.com">Google</a>`;
+    }
+}
--- readMe.md
+++ readMe.md
@@ -1,1 +1,21 @@
 This is a jsDomWarmup project
+WARMUP:
+1. Create a folder called JsDomWarmup, connect to Git / GitHub
+2. Add an index.html and app.js file, link them together
+3. Configure your debugger.
+4. Add in at least 10 content tags to your HTML and ensure you have:
+4.1. At least 2 images
+4.2. At least 5 unique id's
+4.3. At least 2 class' shared between at least 5 tags
+5. Write a DOM selector that will select a single element based on ID
+6. Change 2 style properties of that element (you pick)
+7. Write a DOM selector that will select all elements with a certain 
+class
+8. Write a loop that will overwrite the innerHTML of each element
+9. Write a DOM selector that will select all images from the page
+10. Write a loop that will change the src of each image to a new image 
+link (you pick one)
+11. Write a DOM selector that will select all p tags from the page
+12. Write a loop that will add an a tag inside the p tags content IF the 
+p tag includes the string `link`
+13. Add, commit and push your code
